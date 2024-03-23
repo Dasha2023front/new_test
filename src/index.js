5230331f4aaf6398dec103fdbf0c31feea646270
Darya Gladneva <davydova_di@mail.ru>
2024-03-23 13:04:42 +0300

    added test

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import "./styles/main.scss";
+
+const btn = document.querySelector(".dropdown__button");
+const dropdownList = document.querySelector(".dropdown__list");
+const listItems = document.querySelectorAll(".dropdown__list-item");
+const inputHidden = document.querySelector(".dropdown__input-hidden");
+
+btn.addEventListener("click", () => {
+  dropdownList.classList.toggle("dropdown__list-visible");
+  btn.classList.toggle("active");
+});
+
+// отправка формы
+
+const submitBtn = document.querySelector(".submit__button");
+const f = document.querySelector(".f__form");
+console.log(f);
+
+f.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(this);
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url, {
+    method: "post",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: data,
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.id === 101) {
+        submitBtn.innerText = "Отправлено";
+      }
+      console.log(json);
+    })
+    .catch((err) => console.log(err));
+});
+
+// Выбор элементов списка, запоминание выбранного значения, закрытие списка
+
+listItems.forEach((listItem) => {
+  listItem.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let target = e.target;
+    console.log(target);
+    document.querySelector(".dropdown__city").innerText = target.innerText;
+    document.querySelector(".dropdown__button").focus();
+    inputHidden.value = target.dataset.value;
+    dropdownList.classList.remove("dropdown__list-visible");
+    btn.classList.remove("dropdown__button-active");
+  });
+});
+
+// Закрытие дропдауна по клику вне списка
+
+document.addEventListener("click", (e) => {
+  if (e.target !== btn) {
+    dropdownList.classList.remove("dropdown__list-visible");
+    btn.classList.remove("dropdown__button-active");
+  }
+});
